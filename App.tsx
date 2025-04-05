53d196a46380b186ce9abc7b9835ac3d3166036e
Biswash908 <biswashdem0987@gmail.com>
2025-04-05 16:44:24 +0545

    Minor tweaking of code

--- App.tsx
+++ App.tsx
@@ -143,7 +143,7 @@ const App: React.FC = () => {
     const prepare = async () => {
       try {
         // Simulate a delay (e.g., 2 seconds)
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        await new Promise(resolve => setTimeout(resolve, 3000))
       } catch (e) {
         console.warn(e)
       } finally {
